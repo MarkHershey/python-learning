b65f4554f62a08620d8d218db053d65fde7ab9c3
Mark Huang <hhzeos@gmail.com>
2020-01-28 23:40:19 +0800

    Update

--- workshop/py101/5_loops-0.py
+++ workshop/py101/5_loops-0.py
@@ -1,2 +1,7 @@
 for i in range(10):
     print(i)
+
+
+for _ in range(10):
+    # print("Hello")
+    pass
--- workshop/py101/6_sets.py
+++ workshop/py101/6_sets.py
@@ -6,4 +6,15 @@ s.add(2)
 s.add(1)
 s.add(0)
 
-print(s)
+print("set s | ", s)
+
+
+odd = set((1, 3, 5, 7, 9, 11))
+even = {2, 4, 6, 8, 10}
+
+s1 = odd.intersection(even)
+print("the intersection of odd and even | ", s1)
+s2 = odd.union(even)
+print("the union of odd and even | ", s2)
+s3 = odd.difference(s)
+print("the difference between s and odd | ", s3)
